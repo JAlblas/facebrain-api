669dfb2d9576dddb0c4d55520c671e124044545e
Jasper Alblas <just_jasper@hotmail.com>
2020-10-19 20:41:18 +0200

    Adds profile endpoint

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.post('/signin', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-    const { email, name, password} = req.body;
+    const { email, name, password } = req.body;
     database.users.push({
             id: '125',
             name: name,
@@ -49,6 +49,20 @@ app.post('/register', (req, res) => {
             joined: new Date()
     })
     res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        } 
+    })
+    if (!found) {
+        res.status(400).json('Not Found!');
+    }
 })
 
 app.listen(3000, () => {
