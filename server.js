254cec25ea5931ee82a6dad276c54b13b5479e84
Jasper Alblas <just_jasper@hotmail.com>
2020-10-29 15:07:57 +0100

    added knex and connection to db

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '',
+        database: 'smart-brain'
+    },
+});
+
+console.log(knex.select('*').from('users'));
 
 const app = express();
 
