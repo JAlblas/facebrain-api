102ebd1fbcc26e097b35a1755834fa64471fcd82
Jasper Alblas <just_jasper@hotmail.com>
2020-11-03 19:58:29 +0100

    minor change

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image = require('./controllers/image');
 const db = require('knex')({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-curved-73991',
         user: 'postgres',
         password: 'postgres',
         database: 'smart-brain'
